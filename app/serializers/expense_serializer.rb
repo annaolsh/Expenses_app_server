ad59940c0628b20ba8810efdb3db34efcb32cb2c
github username <github email address>
2017-05-05 15:52:45 -0400

    done

--- app/serializers/expense_serializer.rb
+++ app/serializers/expense_serializer.rb
@@ -1,5 +1,5 @@
 class ExpenseSerializer < ActiveModel::Serializer
-  attributes :product, :cost, :quantity, :description, :date
+  attributes :product, :cost, :quantity, :description, :dateg
 
   def date
     object.created_at.asctime.split(" ")[0]
